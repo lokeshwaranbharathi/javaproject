00cffe688a12796696b11612b5ff8851ffc2b876
lokeshwaranbharathi <lokeshwaranbharathi035@gmail.com>
2021-11-30 15:42:19 +0530

    New commit

---
+++ src/com/java/org/For.java
@@ -0,0 +1,14 @@
+package com.java.org;
+
+public class For {
+ public static void main(String[] args) {
+	
+	 for (int i = 0; i <=3; i++) {
+		for (int j = 0; j <=3; j++) {
+			System.out.print(i);
+		}
+		System.out.println();
+	}
+}
+	
+}
---
+++ src/com/java/org/While.java
@@ -0,0 +1,12 @@
+package com.java.org;
+
+public class While {
+
+	public static void main(String[] args) {
+		int i =0;
+		while (i<=5) {
+			System.out.println(i);
+			i++;
+		}
+	}
+}
